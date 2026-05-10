635af833407cb8811872afcf429b46eeb7385202
wenyunzhe <wenyunzhe@guanxinlab.com>
2026-05-10 21:31:05 +0800

    refactor: remove framework-specific dependencies from common package
    Remove Gin, Kratos, and gRPC adapter code to keep common package
    framework-agnostic. Services should import official otel adapters
    directly when needed.

--- trace/defs/tracer.go
+++ trace/defs/tracer.go
@@ -1,22 +1,8 @@
 package defs
 
-import (
-	"context"
-	"net/http"
-
-	"google.golang.org/grpc"
-)
+import "context"
 
 type Tracer interface {
 	TraceID(ctx context.Context) string
 	SpanID(ctx context.Context) string
-}
-
-type HTTPMiddleware interface {
-	Handler(next http.Handler) http.Handler
-}
-
-type GRPCInterceptor interface {
-	ServerOptions() []grpc.ServerOption
-	ClientOptions() []grpc.DialOption
 }
